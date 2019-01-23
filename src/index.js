4835e8c0edc6756a6112f50020999dc64fe54709
PKrishnakumar <PKrishnakumar@webmd.net>
2019-01-23 15:46:36 -0500

    separated class for each componenets

--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom'
+import Game from './game';
 import './index.css';
-
-class Square extends React.Component {
-    render(){
-        return (
-            <button className="square">
-                {/* to do   */}
-            </button>
-        );
-    }
-}
-class Board extends React.Component {
-    renderSquare(i){
-        return <Square />;
-    }
-    render(){
-        return <div>
-            <div className="status"> {/* to do   */}</div>
-            <div className="board-row">
-              {this.renderSquare(0)}
-              {this.renderSquare(1)}
-              {this.renderSquare(2)}
-            </div>
-            <div className="board-row">
-              {this.renderSquare(3)}
-              {this.renderSquare(4)}
-              {this.renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-            </div>            
-          </div>;
-    }
-}
-class Game extends React.Component {
-    render (){
-        return (
-            <div className="game">
-                <div class="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div> {/* status   */} </div>
-                    <ol>  {/* to do   */} </ol>
-                </div>
-            </div>
-        );
-    }
-}
 
 ReactDom.render(<Game />, document.getElementById('root'));
---
+++ src/square.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Square extends Component {
+    render() {
+        return (
+            <button className="square">
+                {/* to do   */}
+            </button>
+        );
+    }
+}
+export default Square;

c2a693e6ba052eae589e2853d6c714e25cb11f1e
PKrishnakumar <PKrishnakumar@webmd.net>
2019-01-24 15:07:55 -0500

    finished the overview section from react tutorial

--- src/square.js
+++ src/square.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 
 class Square extends Component {
+    constructor(props){
+        super(props);
+        this.state = {value: null };
+    }
+    handleClick() {
+        console.log(this);
+        this.setState({value: "x"});
+    }    
     render() {
+        //console.log(this.props);
+
         return (
-            <button className="square">
-                {/* to do   */}
+            <button className="square" 
+            onClick={ ()=>this.handleClick()}>      
+            {this.state.value}         
             </button>
         );
     }
 }
+
 export default Square;
